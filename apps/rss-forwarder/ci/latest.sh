b4b7a78f7730ab5fb2c915d57b4e3db6f6140912
Jory Irving <jory@jory.dev>
2024-01-21 12:18:04 -0700

    try again

--- apps/rss-forwarder/ci/latest.sh
+++ apps/rss-forwarder/ci/latest.sh
@@ -2,4 +2,5 @@
 version="$(curl -sX GET "https://api.github.com/repos/morphy2k/rss-forwarder/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)"
 version="${version#*v}"
 version="${version#*release-}"
+version="0.7.0-beta.2" #hardcode this for now
 printf "%s" "${version}"
