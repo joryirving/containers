75a9697d1fe13bd9dcf4876de8ffa98af9269585
Jory Irving <jory@jory.dev>
2024-01-21 08:30:23 -0700

    fix tag

--- apps/feedcord/ci/latest.sh
+++ apps/feedcord/ci/latest.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-version=$(curl -H "Accept: application/vnd.github+json" https://api.github.com/repos/Qolors/FeedCord/releases | jq  -r 'sort_by(.published_at) | reverse | .[].name | select( index("CLI") )' | sed 's:.*CLI v::' | head -n 1)
+version="$(curl -sX GET "https://api.github.com/repos/Qolors/FeedCord/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)"
 version="${version#*v}"
+version="${version#*release-}"
 printf "%s" "${version}"
