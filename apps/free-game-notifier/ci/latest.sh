d24e9f6f4ffd77b71f6c12d48b72bfa2601094e1
Jory Irving <jory@jory.dev>
2024-03-07 07:45:52 -0700

    chore: update free-game-notifier to semantic versioning

--- apps/free-game-notifier/ci/latest.sh
+++ apps/free-game-notifier/ci/latest.sh
@@ -2,5 +2,4 @@
 version="$(curl -sX GET "https://api.github.com/repos/j6nca/free-game-notifier/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)"
 version="${version#*v}"
 version="${version#*release-}"
-version="latest" # hardcoded until semantic versioning is implemented
 printf "%s" "${version}"
