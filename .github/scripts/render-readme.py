850446b336035d96a685a996040cb167987efc15
Jory Irving <jory@jory.dev>
2024-10-07 10:40:06 -0600

    chore: update render-readme

--- .github/scripts/render-readme.py
+++ .github/scripts/render-readme.py
@@ -4,20 +4,16 @@ import yaml
 import logging
 from jinja2 import Environment, PackageLoader, select_autoescape
 
-# Set up logging
 logging.basicConfig(level=logging.INFO)
-# Load repo owner and name from environment variables
 repo_owner = os.getenv('REPO_OWNER') or os.getenv('GITHUB_REPOSITORY_OWNER', 'default_owner')
 repo_name = os.getenv('REPO_NAME') or os.getenv('GITHUB_REPOSITORY', 'default_repo')
 
-# Initialize Jinja2 environment
 env = Environment(
     loader=PackageLoader("render-readme"),
     autoescape=select_autoescape()
 )
 
-# Function to load YAML metadata
-def load_metadata_file_yaml(file_path):
+def load_metadata(file_path):
     try:
         with open(file_path, "r") as f:
             return yaml.safe_load(f)
@@ -27,17 +23,16 @@ def load_metadata_file_yaml(file_path):
         logging.error(f"File {file_path} not found.")
     return None
 
-# Function to process metadata and collect images
-def process_metadata_files(apps_dir):
+def process_metadata(apps_dir):
     app_images = []
     for subdir, _, files in os.walk(apps_dir):
-        # Look for metadata.yaml
         if "metadata.yaml" not in files:
-            continue
-        # Load YAML metadata
-        meta = load_metadata_file_yaml(os.path.join(subdir, "metadata.yaml"))
+            continue # Skip if metadata file not found
+
+        meta = load_metadata(os.path.join(subdir, "metadata.yaml"))
         if not meta:
-            continue  # Skip if metadata couldn't be loaded
+            continue # Skip if metadata couldn't be loaded
+
         # Iterate through the channels and build image metadata
         for channel in meta.get("channels", []):
             name = meta["app"] if channel.get("stable", False) else f"{meta['app']}-{channel['name']}"
@@ -50,3 +45,14 @@ def process_metadata_files(apps_dir):
             app_images.append(image)
             logging.info(f"Added image {name} from channel {channel['name']}")
     return app_images
+
+if __name__ == "__main__":
+    apps_dir = "./apps"
+    app_images = process_metadata(apps_dir)
+    try:
+        template = env.get_template("README.md.j2")
+        with open("./README.md", "w") as f:
+            f.write(template.render(app_images=app_images))
+        logging.info("README.md successfully generated.")
+    except Exception as e:
+        logging.error(f"Error rendering template: {e}")
