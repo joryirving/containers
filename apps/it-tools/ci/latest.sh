1800907b7ab529ee465fdca4ada567e2795ab498
Jory Irving <jory@jory.dev>
2024-10-11 09:04:06 -0600

    fix: it-tools versioning

--- apps/it-tools/ci/latest.sh
+++ apps/it-tools/ci/latest.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 version="$(curl -sX GET "https://api.github.com/repos/CorentinTh/it-tools/releases/latest" | jq --raw-output '.tag_name' || true)"
-fullversion="${version}"
 version="${version#*v}"
 version="${version#*release-}"
 version="${version%%-*}"
 printf "%s" "${version}"
-printf "%s" "${fullversion}"
