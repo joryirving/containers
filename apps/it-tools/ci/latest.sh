b56f2ed0f2d21a00956af6b97de8e801385debe0
Jory Irving <jory@jory.dev>
2025-03-17 14:33:02 -0600

    feat: the new "beat"

--- apps/it-tools/ci/latest.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-version="$(curl -sX GET "https://api.github.com/repos/CorentinTh/it-tools/releases/latest" | jq --raw-output '.tag_name' || true)"
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
