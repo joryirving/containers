07be4162c4aa06bd21bae6ff2f1db7d604658595
Jory Irving <jory@jory.dev>
2025-04-02 13:10:31 -0600

    feat: update qb entrypoint

--- apps/qbittorrent/entrypoint.sh
+++ apps/qbittorrent/entrypoint.sh
@@ -4,15 +4,10 @@
 CONFIG_FILE="/config/qBittorrent/qBittorrent.conf"
 LOG_FILE="/config/qBittorrent/logs/qbittorrent.log"
 
-# Set environment variables (QBITTORRENT__PORT and QBITTORRENT__BT_PORT are deprecated)
-export QBT_WEBUI_PORT="${QBITTORRENT__PORT:-$QBT_WEBUI_PORT}"
-export QBT_TORRENTING_PORT="${QBITTORRENT__BT_PORT:-$QBT_TORRENTING_PORT}"
-
 # Ensure the config file exists, copy default if missing
 if [[ ! -f "${CONFIG_FILE}" ]]; then
-    echo "Copying over default configuration ..."
     mkdir -p "${CONFIG_FILE%/*}"
-    cp /app/qBittorrent.conf "${CONFIG_FILE}"
+    cp /defaults/qBittorrent.conf "${CONFIG_FILE}"
 fi
 
 # Set up log file to redirect to stdout
@@ -22,4 +17,4 @@ if [[ ! -f "${LOG_FILE}" ]]; then
 fi
 
 # Execute qBittorrent
-exec /app/qbittorrent-nox "$@"
+exec /usr/bin/qbittorrent-nox "$@"
