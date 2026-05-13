4c82151d33f277b8fef32e8da9f0bf0763fdef03
Jory Irving <jory@jory.dev>
2026-05-12 20:55:54 -0600

    fix(kopia): add required flag to bind non-localhost

--- apps/kopia/entrypoint.sh
+++ apps/kopia/entrypoint.sh
@@ -7,6 +7,7 @@ if [[ ${KOPIA_WEB_ENABLED} == "true" ]]; then
             start \
             --insecure \
             --address "0.0.0.0:${KOPIA_WEB_PORT}" \
+	        --allow-extremely-dangerous-unauthenticated-server-on-the-network \
             "$@"
 else
     exec \
