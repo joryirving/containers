2c5e3f18c215a8cbafdea8c58ee11eaf4ab74f03
Jory Irving <46251616+joryirving@users.noreply.github.com>
2026-06-12 14:55:01 -0600

    Refactor workflows and files based on upstream (#718)
    * catch up on stuff
    
    * bug fixes

--- apps/agentmemory/container_test.go
+++ apps/agentmemory/container_test.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"context"
 	"testing"
 
 	"github.com/joryirving/containers/testhelpers"
 )
 
 func Test(t *testing.T) {
-	ctx := context.Background()
 	image := testhelpers.GetTestImage("ghcr.io/joryirving/agentmemory:rolling")
-	testhelpers.TestHTTPEndpoint(t, ctx, image, testhelpers.HTTPTestConfig{
+	testhelpers.TestHTTPEndpoint(t, image, testhelpers.HTTPTestConfig{
 		Port:       "3111",
 		Path:       "/agentmemory/livez",
 		StatusCode: 200,
--- apps/hoymiles-exporter/container_test.go
+++ apps/hoymiles-exporter/container_test.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"context"
 	"testing"
 
 	"github.com/joryirving/containers/testhelpers"
 )
 
 func Test(t *testing.T) {
-	ctx := context.Background()
 	image := testhelpers.GetTestImage("ghcr.io/joryirving/hoymiles-exporter:rolling")
 
-	testhelpers.TestHTTPEndpoint(t, ctx, image, testhelpers.HTTPTestConfig{
+	testhelpers.TestHTTPEndpoint(t, image, testhelpers.HTTPTestConfig{
 		Port: "9099",
 		Path: "/metrics",
 	}, &testhelpers.ContainerConfig{
